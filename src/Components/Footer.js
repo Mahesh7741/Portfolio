bcb4c9066e836425f91c5046713da30a39123546
Mahesh Savant <SMAHESH7741@GMAIL.COM>
2024-12-07 20:31:26 +0530

    chage new

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
       <h4>Developed by Mahesh Savant</h4>
       <h4>Copyright &copy; 2023 MS</h4>
       <div className='footerLinks'>
-        <a href="https://github.com/DevanshSahni" target='_blank'><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/devansh-sahni/" target='_blank'><FaLinkedin/></a>
-        <a href='mailTo:devanshsahni@gmail.com' target='_blank'><GrMail/></a>
-        <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a>
+        <a href="https://github.com/Mahesh7741" target='_blank'><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/mahesh-savant" target='_blank'><FaLinkedin/></a>
+        <a href='mailTo:maheshsavant8799@gmail.com' target='_blank'><GrMail/></a>
+        <a href="https://leetcode.com/u/MaheshSavant/" target="_blank"><SiLeetcode/></a>
       </div>
     </footer>
   )
